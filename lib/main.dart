79ba710379c3ef59076b075c4e2de7a4e419e47a
TomoyaShibata <wind.of.hometown@gmail.com>
2018-02-13 13:05:32 +0900

    Add users list by dammy data

--- lib/main.dart
+++ lib/main.dart
@@ -28,43 +28,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    var listTile = new ListTile(
+      leading: new CircleAvatar(
+        backgroundImage: new NetworkImage(
+            'https://avatars3.githubusercontent.com/u/2193123?v=4'),
+      ),
+      title: const Text('Tomoya Shibata'),
+    );
+
     return new Scaffold(
       appBar: new AppBar(
-//        title: new Text(widget.title),
         title: new TextField(
           decoration: new InputDecoration(
             hintText: 'Search keyword...',
           ),
         ),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: new ListView.builder(
+        padding: new EdgeInsets.symmetric(
+          vertical: 8.0,
+          horizontal: 4.0,
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
+        itemExtent: 70.0,
+        itemCount: 30,
+        itemBuilder: (BuildContext context, int index) => listTile,
       ),
     );
   }
