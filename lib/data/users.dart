14cf9845da891f2bf2f45164e86dc3a2655b252b
TomoyaShibata <wind.of.hometown@gmail.com>
2018-02-14 15:33:37 +0900

    Add Users model class

---
+++ lib/data/users.dart
@@ -0,0 +1,15 @@
+import 'package:search_github_flutter/data/user.dart';
+
+class Users {
+  final int totalCount;
+  final bool incompleteResults;
+  final List<User> items;
+
+  Users(this.totalCount, this.incompleteResults, this.items);
+
+  static Users fromJson(Map<String, dynamic> json) => new Users(
+        json['total_count'],
+        json['incomplete_results'],
+        json['items'].map((item) => new User.fromJson(item)).toList(),
+      );
+}
